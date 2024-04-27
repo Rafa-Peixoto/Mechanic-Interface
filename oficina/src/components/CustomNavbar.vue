a576afe0636e9141a980dd6422aa45c222e31296
Rafa-Peixoto <108181671+Rafa-Peixoto@users.noreply.github.com>
2024-04-27 17:51:36 +0000

    atualizado nav bar

--- oficina/src/components/CustomNavbar.vue
+++ oficina/src/components/CustomNavbar.vue
@@ -9,6 +9,9 @@
           <router-link to="/ServicosTerminados">Serviços Terminados</router-link>
         </div>
       </div>
+      <div class="logout">
+          <router-link to="/Login">Logout</router-link>
+      </div>
     </nav>
   </template>
 
@@ -38,7 +41,7 @@ export default {
   justify-content: space-between;
   align-items: center;
   padding: 10px 10px;
-  background-color: #006400;
+  background-color: #86ff86;
   width: 100%;
   position: relative;
   top: -8px;
@@ -64,13 +67,13 @@ a {
   transition: all 0.3s ease-in;
   margin: 5px;
   font-size: 20px;
-  background-color: #005c00;
+  background-color: #86ff86;
 }
 
 a.router-link-active {
   background-color: var(--color-accent);
   color: var(--color-light);
-  background-color: #005c00;
+  background-color: #86ff86;
 }
 
 a:hover{
@@ -82,4 +85,14 @@ img {
   height: 60px; /* ou qualquer outra altura que você preferir */
 }
 
+.logout a{
+    display: flex;
+    flex-wrap: wrap;
+    color: black;
+    border: none;
+  }
+  .logout a:hover{
+    cursor: pointer;
+    background-color: #a9fdac;
+  }
 </style>
